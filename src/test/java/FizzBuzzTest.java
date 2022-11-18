21ab7f043b4969a4c71540385ac77c64c1c4df80
AlekseiFlow <seva8@tut.by>
2022-11-18 21:15:45 +0200

    FizzBuzz algo and Tests added

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -48,7 +48,7 @@ public class FizzBuzzTest {
 
 
     @Test   // рабочий тест отключен.
-    // специальная аннотация для отключения тестов как рабочих, так и не доделанных до конца.
+    @Ignore // специальная аннотация для отключения тестов как рабочих, так и не доделанных до конца.
     public void testStartLessThanEnd_StartEndNegative_HappyPath() {
         //AAA
         //arange
@@ -77,7 +77,7 @@ public class FizzBuzzTest {
         //arrange
         int start = 20;
         int end = 1;
-        String[] epectedResult = {};
+        String[] epectedResult = {"1"};
 
         //act
         String[] actualResult = new FizzBuzz().fizzBuzz(start, end);
